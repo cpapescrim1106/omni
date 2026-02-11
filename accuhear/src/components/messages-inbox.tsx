9a3c0cb1324a11890ec9d289e6f9c32fd12ee9ad
Chris <cppape@gmail.com>
2026-02-10 20:40:19 -0500

    Set messages inbox height to 100vh

--- accuhear/src/components/messages-inbox.tsx
+++ accuhear/src/components/messages-inbox.tsx
@@ -162,7 +162,7 @@ export function MessagesInbox() {
   return (
     <section
       className="card flex min-h-0 flex-col p-6"
-      style={{ height: "90vh" }}
+      style={{ height: "100vh" }}
       data-testid="messages-inbox"
     >
       <div className="flex flex-wrap items-center justify-between gap-4">
